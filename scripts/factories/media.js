0654fb429d90abf86d733069453f229915132f45
Jordy Saint-Albin <jordy.saintalbin@MacBook-Pro-1.local>
2023-05-23 00:14:23 +0200

    fix-image-card

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,18 +1,23 @@
 export function mediaFactory(data) {
-  const { title, id, image, video, likes, date, price, photographerId, name } =
-    data;
-  const Image = `assets/photographers/${photographerId}/${image}`;
+  const { title, id, image, video, likes, date, price, photographerId } = data;
+  const Image = `assets/images
+  /${photographerId}/${id}/${image}`;
+  const Video = `assets/images
+  /${photographerId}/${video}`;
   function getMediaCardDOM() {
     // Create an article element
     const article = document.createElement("article");
     article.id = id;
 
-    const isName = document.createElement("h2");
-    isName.textContent = name;
-
-    const isImage = document.createElement("photographerImage");
-    isImage.classList.add("photographerImage");
+    // Create a image element for the artist image
+    const isImage = document.createElement("img");
+    isImage.classList.add("image");
     isImage.setAttribute("src", Image);
+    isImage.setAttribute("alt", title);
+
+    // Create a image element for the artist image
+    const isVideo = document.createElement("video");
+    isVideo.setAttribute("source", Video);
 
     // Create a likes element for the artist likes
     const isLikes = document.createElement("likes");
@@ -32,10 +37,12 @@ export function mediaFactory(data) {
     isPrice.textContent = `${price}€/jour`;
 
     // append  an Image, a date, a likes, a title,  an image to the article element
+    article.appendChild(isVideo);
     article.appendChild(isImage);
     article.appendChild(isDate);
     article.appendChild(isLikes);
     article.appendChild(isPrice);
+
     return article;
   }
   return { getMediaCardDOM };
