6eb1393fa7cc1384bf2066a2cedbda9d31d4378a
Jordy Saint-Albin <jordy.saintalbin@MacBook-Pro-1.local>
2023-08-07 13:38:56 +0200

    form-validation2

--- scripts/factories/formValidation.js
+++ scripts/factories/formValidation.js
@@ -1,37 +1,108 @@
-form.addEventListener("submit", (e) => {
-	// Prevent submit form
-	e.preventDefault();
-	// Fetch all the forms we want to apply custom Bootstrap validation styles to
-	const form = $("#form")[0];
-	const firstName = $("#first")[0];
-	const lastName = $("#last")[0];
-	const email = $("#email")[0];
-	const message = $("#message")[0];
-	const confirmation = $("#confirmation")[0];
 
-	// Regex for form validation
-	const nameRegex = /^([A-Za-zÀ-ÖØ-öø-ÿ][A-Za-zÀ-ÖØ-öø-ÿ ,.'-]*){2}$/g;
-	const emailRegex = /(?=^.{5,255}$)^([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,})$/g;
-	let formValidated = true; // Need to be true to submit form
+  
+  // DOM Elements
+  const form = document.getElementById("form");
+  const errorMsg = document.querySelectorAll(".invalid-feedback");
+  const confirmation = document.getElementById("confirmation");
+  const btnSubmit = document.getElementById("submitValidation");
 
-	// Function to check form inputs with regex and add errors messages if inputs are not validated
-	function validateInput(input, regex) {
-		const validated = input.value.match(regex);
-		// If input is not validated : formValidated="false"
-		if (!validated) {
-			formValidated = false;
-		}
-	}
+  const firstName = document.getElementById("firstName");
+  const lastName = document.getElementById("lastName");
+  const email = document.getElementById("email");
 
-	validateInput(firstName, nameRegex);
-	validateInput(lastName, nameRegex);
-	validateInput(email, emailRegex);
+  
 
-	// Show confirmation message and hide form if form is validated
-	if (form.checkValidity() && formValidated) {
-		confirmation.style.display = "block";
-		form.style.display = "none";
-		console.log("Prenom : " + firstName.value + ", Nom : " + lastName.value + ", Email : " + email.value + ", Message : " + message.value);
-	}
-	form.classList.add("was-validated");
-});
+  const message = document.getElementById("message");
+  
+  
+  const Identityregex = /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/;
+  
+  const mailFormatRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  
+  
+  console.log("btnSubmit",btnSubmit)
+  // submit modal event
+  form.addEventListener("submit", checkValidation);
+  
+
+  // check input form
+  //check identityName
+  
+  function checkIdentityName(identityName, index) {
+ 
+   
+    if (
+      identityName.value.trim().length < 2 ||
+      identityName.value.trim() === "" ||
+      !identityName.value.match(Identityregex)
+    ) 
+    
+    {
+      errorMsg[index].style.display = "block";
+      return false;
+    }
+    errorMsg[index].style.display = "none";
+    return true;
+  }
+ 
+  // check email
+  function checkEmail() {
+    console.log("email",email);
+    if (mailFormatRegex.test(email.value) === false) {
+      errorMsg[2].style.display = "block";
+      return false;
+    }
+    errorMsg[2].style.display = "none";
+    return true;
+  }
+  
+
+  
+  
+  // check Message
+  
+  function checkMessage() {
+    console.log("message",message)
+    if (
+        message.value.trim().length < 20 ||
+        message.value.trim() === "" ||
+        !message.value.match(Identityregex)
+      ) {
+        errorMsg[3].style.display = "block";
+        return false;
+      }
+      errorMsg[3].style.display = "none";
+      return true;
+  }
+  
+ 
+  
+  function checkValidation(event) {
+    console.log("XXXXXXXXX", "CHECK VALIATION")
+    event.preventDefault();
+  
+    const isCheckFirstName = checkIdentityName(firstName, 0);
+  
+    const iscCheckLastName = checkIdentityName(lastName, 1);
+  
+    const isCheckEmail = checkEmail();
+  
+    const ischeckMessage = checkMessage();
+    console.log("checkvalidation", isCheckFirstName,
+  iscCheckLastName,
+  isCheckEmail,
+  ischeckMessage)
+    if (
+      isCheckFirstName === true &&
+      iscCheckLastName === true &&
+      isCheckEmail === true &&
+      ischeckMessage === true
+    )
+      // We display the modal of the successful message.
+   {
+    confirmation.style.display="block";
+     console.log("Prenom : " + identityName.value + ", Nom : " + identityName.value + ", Email : " + email.value + ", Message : " + message.value);
+    }
+    return false
+    // return  form.style.display="block";
+  }
